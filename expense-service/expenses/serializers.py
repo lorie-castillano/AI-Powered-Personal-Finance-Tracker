9e624c0884200e502789e7d74e88568b51481b9b
Lorie Castillano <clorieanne@gmail.com>
2025-03-12 14:44:45 +0800

    Remove auth-service and expense-service.

--- expense-service/expenses/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from expenses.models import Expense
-
-
-class ExpenseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Expense
-        fields = "__all__"
